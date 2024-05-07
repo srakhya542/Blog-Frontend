0085cf80d1189995756b39ae2fd7db35e93a6b84
srakhya542 <117500558+srakhya542@users.noreply.github.com>
2024-05-07 20:22:49 +0530

    Add files via upload

---
+++ components/Posts.jsx
@@ -0,0 +1,41 @@
+import React ,{useEffect,useState}from 'react'
+import axios from 'axios'
+import PostItem from './PostItem'
+import Loader from '../components/Loader'
+
+
+const Posts = () => {
+    const[posts,setPosts]=useState([])
+      const [isLoading,setIsLoading] = useState(false)
+    
+  useEffect(() =>{
+    const fetchPosts = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts`)
+        setPosts(response?.data)
+      } catch (err) {
+        console.log(err)
+      }
+      setIsLoading(false)
+    }
+    fetchPosts();
+  },[])
+  if(isLoading) {
+    return <Loader/>
+  }
+
+
+  return (
+   <section className="posts">
+     {posts.length>0 ?<div className="container posts__container">
+        {
+            posts.map(({_id: id,thumbnail,category,title,description,creater,createdAt}) => <PostItem key={id}  postID={id} thumbnail={thumbnail} category={category} title={title}
+            description={description} authorID={creater} createdAt={createdAt}/>)
+        }
+    </div>: <h2 className='center'>No posts found</h2>}
+   </section>
+  )
+}
+
+export default Posts
---
+++ pages/AuthorPosts.jsx
@@ -0,0 +1,43 @@
+import React, {useState,useEffect}from 'react'
+import PostItem from '../components/PostItem'
+import Loader from '../components/Loader'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+const AuthorPosts = () => {
+  const[posts,setPosts]=useState([])
+  const [isLoading,setIsLoading] = useState(false)
+
+  const {id} = useParams()
+
+useEffect(() =>{
+const fetchPosts = async () => {
+  setIsLoading(true);
+  try {
+    const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/users/${id}`)
+    setPosts(response?.data)
+  } catch (err) {
+    console.log(err)
+  }
+  setIsLoading(false)
+}
+fetchPosts();
+},[id])
+if(isLoading) {
+return <Loader/>
+}
+
+
+return (
+<section className="posts">
+ {posts.length>0 ?<div className="container posts__container">
+    {
+        posts.map(({_id: id,thumbnail,category,title,description,creater,createdAt}) => <PostItem key={id}  postID={id} thumbnail={thumbnail} category={category} title={title}
+        description={description} authorID={creater} createdAt={createdAt}/>)
+    }
+</div>: <h2 className='center'>No posts found</h2>}
+</section>
+)
+}
+
+export default AuthorPosts
