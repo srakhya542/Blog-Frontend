583fca2ba0c6ef5b73a83af7d11fa34ebe18222a
srakhya542 <117500558+srakhya542@users.noreply.github.com>
2024-05-14 19:31:31 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import Thumbnail1 from './images/blog1.jpg'
+import Thumbnail2 from './images/blog2.jpg'
+import Thumbnail3 from './images/blog3.jpg'
+import Thumbnail4 from './images/blog4.jpg'
+export const DUMMY_POSTS=[
+    {
+    id:'1',
+    thumbnail:Thumbnail1,
+    category:'education',
+    title:'The Rise of Online Learning',
+    desc:"In today's fast-paced world online learning has become increasingly popular among students of all ages. This blog post delves into the myriad benefits of online education from flexibility and convenience to access to a wide range of courses.With online learning students have the flexibility to study at their own pace and on their own schedule. Whether balancing work, family, or other commitments online courses offer the convenience of learning from anywhere at any time. This flexibility is particularly advantageous for adult learners looking to further their education while juggling various responsibilities",
+    authorID:3
+    },
+    {
+     id:'2',
+    thumbnail:Thumbnail2,
+    category:'science',
+    title:'Unraveling the Mysteries of Quantum Mechanics',
+    desc:'Dive into the fascinating world of quantum mechanics in this captivating blog post. Explore the mind-bending concepts of superposition, entanglement, and quantum computing. Join us as we journey into the realm of the microscopic and unravel the secrets of the universe!',
+    authorID:1
+    },
+    {
+        id:'3',
+        thumbnail:Thumbnail3,
+        category:'weather',
+        title:'Decoding the Dynamics of Weather Patterns',  
+        desc:'Step into the ever-changing world of meteorology with our latest blog post. Journey through the intricate web of atmospheric phenomena as we unravel the dynamics behind weather patterns. From the gentle breezes of spring to the ferocious storms of winter, explore the forces at play that shape our daily forecasts.',
+        authorID:13
+    },
+    {id:'4',
+    thumbnail:Thumbnail4,
+    category:'farming',
+    title:' Innovations Transforming Modern Farming Practices',
+    desc:'Embark on a journey through the evolving landscape of agriculture in our latest blog post. Explore the cutting-edge innovations and technologies revolutionizing modern farming practices, from precision agriculture and vertical farming to sustainable farming methods and agricultural drones.',
+    authorID:11
+},
+
+]
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link}from 'react-router-dom'
+const ErrorPage = () => {
+  return (
+    <section className='error-page'>
+      <div className="center">
+        
+        <h2>Page Not Found</h2>
+        <Link to ="/" className='btn primary'>Go Back Home</Link>
+      </div>
+
+    </section>
+  )
+}
+
+export default ErrorPage
